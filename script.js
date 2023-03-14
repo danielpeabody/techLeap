207e688e87e6fd954d15594e58e63d8b9391e0b0
danielpeabody <peabodydaniel1@gmail.com>
2023-03-13 19:06:36 -0700

    csv reader start

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const csv = require('csv-parser');
+const fs = require('fs');
+const { it } = require('node:test');
+const results = []
+
+fs.createReadStream('jobs.csv')
+.pipe(csv({}))
+.on('data',(data) => results.push(data))
+.on('end', ()=>{
+    console.log('done');
+});
+
+setTimeout(() => {
+    for(let i = 0; i < results.length; i++){
+        console.log(results[i]);
+    }
+},1000)
