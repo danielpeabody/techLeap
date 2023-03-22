0706fc89c216e9d266c3dd05bc427d79a3a6ca77
danielpeabody <peabodydaniel1@gmail.com>
2023-03-22 11:16:22 -0700

    creating company nodes on job page

--- public-html/script.js
+++ public-html/script.js
@@ -1,9 +1,17 @@
 
+
+let intro = document.getElementsByClassName('intro-container2');
+
 let url = 'http://localhost:3000/jobs'
 let p = fetch(url);
 p.then((response) => response.json())
 .then((data) => {
-    console.log(data);
-    console.log('test');
+    for(let i = 0; i < data.length; i ++){
+        const node = document.createElement("h2");
+        const textnode = document.createTextNode(data[i].company);
+        node.appendChild(textnode);
+        intro[0].appendChild(node);
+
     }
+}
 )
