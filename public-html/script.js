b884a12fdc3c716d10506e642c142eaad3bb34aa
danielpeabody <peabodydaniel1@gmail.com>
2023-03-26 17:08:29 -0700

    pull all data from csv to jobs page

--- public-html/script.js
+++ public-html/script.js
@@ -7,8 +7,9 @@ let p = fetch(url);
 p.then((response) => response.json())
 .then((data) => {
     for(let i = 0; i < data.length; i ++){
+        let keylist = Object.keys(data[0]);
         const node = document.createElement("h2");
-        const textnode = document.createTextNode(data[i].company);
+        const textnode = document.createTextNode(data[i][keylist[0]] + " | " + data[i][keylist[1]] + " | " +  data[i][keylist[2]] + " | " + data[i][keylist[4]] + " | " + data[i][keylist[5]] + " | " + data[i][keylist[6]]);
         node.appendChild(textnode);
         intro[0].appendChild(node);
 
